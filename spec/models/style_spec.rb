402c3b33576a59fbb4c0f85c487c6540e27a1dfb
Jonah <jk51287@gmail.com>
2013-12-18 15:13:08 -0500

    Added style model and validation tests.

---
+++ spec/models/style_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Style do
+  let(:blank_values) { [nil, ''] }
+  it { should have_valid(:name).when('Modern') }
+  it { should_not have_valid(:name).when(*blank_values) }
+end
