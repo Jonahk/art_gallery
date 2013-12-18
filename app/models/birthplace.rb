3772ff1c786f1adb4fe3f55d5855648e489366c7
Jonah <jk51287@gmail.com>
2013-12-18 17:18:35 -0500

    Artist model and validation and relational tests.

--- app/models/birthplace.rb
+++ app/models/birthplace.rb
@@ -1,3 +1,4 @@
 class Birthplace < ActiveRecord::Base
   validates_presence_of :city, :country
+  has_many :artist
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,23 +1,23 @@
-FactoryGirl.define do
+# FactoryGirl.define do
 
-  factory :style do
-     name 'Art Deco'
-  end
+#   factory :style do
+#      name 'Art Deco'
+#   end
 
-  factory :medium do
-    name 'Performance Art'
-  end
+#   factory :medium do
+#     name 'Performance Art'
+#   end
 
-  factory :birthplace do
-    city 'Seattle'
-    state 'Washington'
-    country 'United States of America'
-  end
+#   factory :birthplace do
+#     city 'Seattle'
+#     state 'Washington'
+#     country 'United States of America'
+#   end
 
-  # factory :artist do
-  # name 'Jonah Kaufman'
-  # contact 'the_best@launchacademy.co'
-  # style, medium, birthplace
-  # end
+#   # factory :artist do
+#   # name 'Jonah Kaufman'
+#   # contact 'the_best@launchacademy.co'
+#   # style, medium, birthplace
+#   # end
 
-end
+# end
