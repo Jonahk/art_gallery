a4f88e5afd228069e593d378211b3412d2aad817
Jonah <jk51287@gmail.com>
2013-12-19 11:31:26 -0500

    Added sales.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131219160115) do
+ActiveRecord::Schema.define(version: 20131219162726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,12 @@ ActiveRecord::Schema.define(version: 20131219160115) do
 
   create_table "media", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "sales", force: true do |t|
+    t.integer  "customer_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -9,4 +9,5 @@ describe Customer do
   it { should_not have_valid(:contact).when(*blank_values) }
 
   it { should have_many(:collections).through(:favorite_collections) }
+  it { should have_many(:sales) }
 end
